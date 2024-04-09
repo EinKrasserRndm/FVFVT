18214949e39d7ff9ac833a8f3e55868037646c04
EinKrasserRndm <smiley.gurke@gmail.com>
2024-04-09 11:13:39 +0200

    edit

---
+++ 2024-04-09/query.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+console.log('here is my query:');
+// TODO
+//
+async function query() {
+    console.log('querying...');
+    let playLists = await prisma.watchlist.findMany({
+        select:{name:true },
+        where:{ benutzerId: 8 },
+    });
+
+    for(let playList of playLists){
+        console.log(playList.name);
+    }
+    
+    console.log("\n");
+    
+    let watchlist = await prisma.watchlist.findMany({
+        select:{Track:true },
+        where:{ benutzerId: 4 },
+    });
+
+    for(let tracks of watchlist){
+        for( let i = 0; i < tracks.Track.length; i++){
+            console.log(tracks.Track[i].name);
+        }
+    }
+}
+
+query().then(() => {
+console.log('done')});

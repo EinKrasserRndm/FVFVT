4512530e6566ce14b7fb818029547c0c985b8326
EinKrasserRndm <smiley.gurke@gmail.com>
2024-05-28 08:58:13 +0200

    untested code

--- PLF5/Teil1/query.js
+++ PLF5/Teil1/query.js
@@ -1,2 +1,82 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+async function getAllZooNames() {
+    const zoos = await prisma.zoo.findMany({
+        select: {
+            id: true,
+            land: true,
+            stadt: true,
+            adresse: true,
+            baujahr: true,
+            name: true,
+        },
+    });
+    return zoos.map(zoo => zoo.name);
+}
+async function getZooById(zooId) {
+    const zoo = await prisma.zoo.findUnique({
+        where: { id: zooId },
+    });
+    return zoo;
+}
+async function getAbteilungenByZooId(zooId) {
+    const abteilungen = await prisma.abteilung.findMany({
+        where: { zooid: zooId },
+    });
+    return abteilungen;
+}
+async function getAbteilungenAndTierCountByZooId(zooId) {
+    const abteilungen = await prisma.abteilung.findMany({
+        where: { zooid: zooId },
+        include: {
+            tiere: true,
+        },
+    });
+
+    return abteilungen.map(abteilung => ({
+        name: abteilung.name,
+        tierCount: abteilung.tiere.length,
+    }));
+}
+async function getMitarbeiterByZooId(zooId) {
+    const mitarbeiter = await prisma.mitarbeiter.findMany({
+        where: {
+            abteilungen: {
+                some: {
+                    zooid: zooId,
+                },
+            },
+        },
+    });
+    return mitarbeiter;
+}
+async function getMitarbeiterAndAbteilungenByZooId(zooId) {
+    const mitarbeiter = await prisma.mitarbeiter.findMany({
+        where: {
+            abteilungen: {
+                some: {
+                    zooid: zooId,
+                },
+            },
+        },
+        include: {
+            abteilungen: true,
+        },
+    });
+
+    return mitarbeiter.map(mitarbeiter => ({
+        name: mitarbeiter.name,
+        abteilungen: mitarbeiter.abteilungen.map(abteilung => abteilung.name),
+    }));
+}
+(async () => {
+    const zooId = '1';
+
+    console.log('Namen aller Zoos:', await getAllZooNames());
+    console.log('Details eines Zoos:', await getZooById(zooId));
+    console.log('Abteilungen eines Zoos:', await getAbteilungenByZooId(zooId));
+    console.log('Abteilungen und Anzahl der Tiere:', await getAbteilungenAndTierCountByZooId(zooId));
+    console.log('Mitarbeiter eines Zoos:', await getMitarbeiterByZooId(zooId));
+    console.log('Mitarbeiter und Abteilungen eines Zoos:', await getMitarbeiterAndAbteilungenByZooId(zooId));
+})();
--- PLF5/Teil1/seed.js
+++ PLF5/Teil1/seed.js
@@ -2,29 +2,103 @@ const { faker } = require('@faker-js/faker');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-
 async function seed() {
+    console.log('Start seeding');
+
+    console.log('Seeding Zoos');
     for (let i = 0; i < 5; i++) {
-        const zoo = await prisma.zoo.create({
+        await prisma.zoo.create({
             data: {
                 land: faker.location.country(),
                 stadt: faker.location.city(),
                 adresse: faker.address.streetAddress(),
-                baujahr: faker.datatype.number({ max: 2024 }),
+                baujahr: faker.datatype.number({ min: 1400, max: 2024 }),
             },
         });
+    }
 
-        for (let j = 0; j < faker.datatype.number({ min: 2, max: 7 }); j++) {
+    console.log('Seeding Abteilungen');
+    const zoos = await prisma.zoo.findMany();
+    for (const current of zoos) {
+        for (let i = 0; i < faker.number.int({ min: 2, max: 7 }); i++) {
             await prisma.abteilung.create({
                 data: {
                     name: faker.animal.type(),
-                    zooid: zoo.id,
+                    zoo: { connect: { id: current.id } },
                 },
             });
         }
     }
-}
-seed()
-    .then ((rw) => console.log('seeding done', rw));
-    
 
+    console.log('Seeding Tiere');
+    const abteilungArr = await prisma.abteilung.findMany();
+    for (const aktuell of abteilungArr) {
+        const tierData = [];
+        for (let i = 0; i < faker.number.int({ min: 5, max: 20 }); i++) {
+            const tierName = faker.animal[aktuell.name.toLowerCase()] ? faker.animal[aktuell.name.toLowerCase()]() : 'unknown'; 
+            tierData.push({
+                name: faker.person.firstName(),
+                art: tierName,
+                abtid: aktuell.id,
+            });
+        }
+        await prisma.tier.createMany({
+            data: tierData,
+        });
+    }
+
+    console.log('Seeding Mitarbeiter');
+    const mitarbeiterIds = [];
+    for (let i = 0; i < 100; i++) {
+        const newMitarbeiter = await prisma.mitarbeiter.create({
+            data: {
+                name: faker.person.firstName(),
+            },
+        });
+        mitarbeiterIds.push(newMitarbeiter.id);
+    }
+
+    console.log('Connecting Mitarbeiter with Abteilungen');
+    const abteilungenByZoo = {};
+    for (const zoo of zoos) {
+        abteilungenByZoo[zoo.id] = await prisma.abteilung.findMany({
+            where: {
+                zooid: zoo.id,
+            },
+        });
+    }
+
+    const updates = [];
+    for (const mitarbeiterId of mitarbeiterIds) {
+        const randomZoo = zoos[faker.number.int({ min: 0, max: zoos.length - 1 })];
+        const abteilungenZoo = abteilungenByZoo[randomZoo.id];
+
+        if (abteilungenZoo.length === 0) continue;
+
+        for (let j = 0; j < faker.number.int({ min: 1, max: 3 }); j++) {
+            const randomAbteilung = abteilungenZoo[faker.number.int({ min: 0, max: abteilungenZoo.length - 1 })];
+            updates.push(prisma.mitarbeiter.update({
+                data: {
+                    abteilung: {
+                        connect: { id: randomAbteilung.id },
+                    },
+                },
+                where: {
+                    id: mitarbeiterId,
+                },
+            }));
+        }
+    }
+
+    await Promise.all(updates);
+}
+
+seed()
+    .then(() => {
+        console.log('Seeding done');
+        prisma.$disconnect();
+    })
+    .catch((error) => {
+        console.error('Seeding error:', error);
+        prisma.$disconnect();
+    });
